49229548bc0bafe341060672a6b7b72b4a2ca4db
John Chris <ici.faq@gmail.com>
2019-06-22 18:58:41 +0300

    Added CSS modules and removed SCSS plugin.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,13 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import headerStyles from './header.module.css';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}>
+  <header className={headerStyles.container}>
+    <div className={headerStyles.content}>
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}>
+        <Link to="/" className={headerStyles.title}>
           {siteTitle}
         </Link>
       </h1>

a5e37b0a1b0ebb2da8952f72f5772087b77bce81
Cristian Iacob <ici.faq@gmail.com>
2019-06-22 15:34:59 +0300

    Changed folder structure

--- src/pages/Page2/index.js
+++ src/pages/Page2/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Layout from '../components/Layout';
-import SEO from '../components/seo';
+import Layout from '../../components/Layout';
+import SEO from '../../components/seo';
+import './style.scss';
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>This is the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>

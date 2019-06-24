d6dd576a14a8b5f48b88100eb35c0747c62325fb
Cristian Iacob <ici.faq@gmail.com>
2019-06-24 20:09:22 +0300

    Added some parallax effect to play with

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -10,6 +10,8 @@ import SEO from '../components/seo';
 import mainPageStyles from './styles/mainPage.module.css';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 
+const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`;
+
 class MainPage extends React.Component {
   render() {
     return (
@@ -18,6 +20,10 @@ class MainPage extends React.Component {
         <div className={mainPageStyles.container}>
           <div>
             <Parallax pages={3} scrolling={true} ref={ref => (this.parallax = ref)}>
+              <ParallaxLayer offset={0} speed={0.5} style={{ backgroundColor: '#805E73' }}></ParallaxLayer>
+              <ParallaxLayer offset={1} speed={0.5} style={{ backgroundColor: '#87BCDE' }}></ParallaxLayer>
+              <ParallaxLayer offset={2} speed={0.5} style={{ backgroundColor: '#20232f' }}></ParallaxLayer>
+              <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }} />
               <ParallaxLayer offset={0} speed={0.5}>
                 <Header />
                 <button onClick={() => this.parallax.scrollTo(1)}>Next</button>

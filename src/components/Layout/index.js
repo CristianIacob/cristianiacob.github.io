1810641b73bc1575f7a2758d5fbe99d5ce70c490
Cristian Iacob <ici.faq@gmail.com>
2019-06-28 17:50:53 +0300

    fixed the layout issues and added transparent img

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,17 +7,13 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
 
-import Footer from '../Footer';
-import Header from '../Header';
 import layoutStyles from './layout.module.css';
 import './layout.css';
 
 const Layout = ({ children }) => {
   return (
     <div className={layoutStyles.wrapper}>
-      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <div className={layoutStyles.container}>
         <main>{children}</main>
       </div>
--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
-import AstronautImage from '../components/OptimizedImages/astronaut';
-import RocketImage from '../components/OptimizedImages/rocket';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import SEO from '../components/seo';
 import mainPageStyles from './styles/mainPage.module.css';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
-
-const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`;
 
 class MainPage extends React.Component {
   render() {
@@ -20,29 +15,29 @@ class MainPage extends React.Component {
         <div className={mainPageStyles.container}>
           <div>
             <Parallax pages={3} scrolling={true} ref={ref => (this.parallax = ref)}>
-              <ParallaxLayer offset={0} speed={0.5} style={{ backgroundColor: '#805E73' }}></ParallaxLayer>
-              <ParallaxLayer offset={1} speed={0.5} style={{ backgroundColor: '#87BCDE' }}></ParallaxLayer>
-              <ParallaxLayer offset={2} speed={0.5} style={{ backgroundColor: '#20232f' }}></ParallaxLayer>
-              <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }} />
+              <ParallaxLayer
+                offset={0}
+                speed={0}
+                factor={3}
+                style={{
+                  backgroundImage: 'url(../images/rocket-transparent.png)',
+                  backgroundSize: 'cover',
+                  backgroundAttachment: 'fixed',
+                }}
+              />
+
               <ParallaxLayer offset={0} speed={0.5}>
                 <Header />
                 <button onClick={() => this.parallax.scrollTo(1)}>Next</button>
-                <section className={mainPageStyles.image}>
-                  <AstronautImage />
-                </section>
               </ParallaxLayer>
-              <ParallaxLayer offset={1} speed={0.5}>
+              <ParallaxLayer offset={1} speed={0.5} style={{ backgroundColor: '#805E73' }}>
+                <div>offset 1</div>
                 <button onClick={() => this.parallax.scrollTo(2)}>Next</button>
-                <section className={mainPageStyles.image}>
-                  <AstronautImage />
-                </section>
               </ParallaxLayer>
-              <ParallaxLayer offset={2} speed={0.5}>
+              <ParallaxLayer offset={2} speed={0.5} style={{ backgroundColor: '#87BCDE' }}>
+                <div>offset 2</div>
                 <section className={mainPageStyles.lastLayer}>
                   <button onClick={() => this.parallax.scrollTo(0)}>Back Top</button>
-                  <section className={mainPageStyles.image}>
-                    <AstronautImage />
-                  </section>
                   <Footer />
                 </section>
               </ParallaxLayer>
